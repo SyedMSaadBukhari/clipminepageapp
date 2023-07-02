9ae0f67a0dbcb303a7b329c87f42f2d52fb094bc
SyedMSaad <saadbukharie@gmail.com>
2023-07-03 03:55:19 +0500

    Hamvburger menu for mobile view added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Sliders from './components/Sliders/Sliders';
-import DiscoverPage from './components/DiscoverPage/DiscoverPage';
-import Features from './components/Features/Features';
-import Benefits from './components/Benefits/Benefits';
-import VideoCollections from './components/VideoCollections/VideoCollections';
-import Partners from './components/Partners/Partners';
-import TagPage from './components/TagPage/TagPage';
-import Platforms from './components/Platforms/Platforms';
-
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Sliders from "./components/Sliders/Sliders";
+import DiscoverPage from "./components/DiscoverPage/DiscoverPage";
+import Features from "./components/Features/Features";
+import Benefits from "./components/Benefits/Benefits";
+import VideoCollections from "./components/VideoCollections/VideoCollections";
+import Partners from "./components/Partners/Partners";
+import TagPage from "./components/TagPage/TagPage";
+import Platforms from "./components/Platforms/Platforms";
 
 function App() {
   return (
-    <div className='clipmine-container'>
-    <Navbar/>
-    <Sliders/>
-    <DiscoverPage/>
-    <Features/>
-    <Benefits/>
-    <VideoCollections/>
-    <Partners/>
-    <TagPage/>
-    <Platforms/>
+    <div className="clipmine-container">
+      <Navbar />
+      <Sliders />
+      <DiscoverPage />
+      <Features />
+      <Benefits />
+      <VideoCollections />
+      <Partners />
+      <TagPage />
+      <Platforms />
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Navbar.scss';
 import logo from "../../clipminepage/clipmine_logo_beta_226x50.png" ;
 
 const Navbar = ()=> {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    const [isMobMenuOpen, setMobMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+        setMobMenuOpen(!isMobMenuOpen);
+    }
     return (
-        <nav className="navbar">
-            <img src={logo} alt='clipmine-logo' className='logo'/>
-            <main className='menu'>
-                <ul className="menu-left">
-                    <li><a className="menu-item" href="#!" >Home</a></li>
-                    <li><a className="menu-item" href="#!">Help</a></li>
-                    <li><a className="menu-item" href="#!">Search</a></li>
-                </ul>
-                
-                <ul className="menu-right">
-                    <li><a className="menu-item" href='#!'>Blog</a></li>
-                    <li><a className="menu-item" href="#!">Log In</a></li>
-                    <li><a className="menu-item" href="#!">Sign Up</a></li>
-                </ul>
-            </main>
-        </nav>
+        <header>
+            <nav className="navbar">
+                <img src={logo} alt='clipmine-logo' className='logo'/>
+                <section className={`menu ${isMenuOpen ? 'open' : ''}`}>
+                    <ul className='menu-left'>
+                        <li><a className="menu-item" href="#!" >Home</a></li>
+                        <li><a className="menu-item" href="#!">Help</a></li>
+                        <li><a className="menu-item" href="#!">Search</a></li>
+                    </ul>
+                    
+                    <ul className='menu-right'>
+                        <li><a className="menu-item" href='#!'>Blog</a></li>
+                        <li><a className="menu-item" href="#!">Log In</a></li>
+                        <li><a className="menu-item" href="#!">Sign Up</a></li>
+                    </ul>
+                    <button className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                        <span className='bar'></span>
+                    </button>
+                </section>
+            </nav>
+            <nav className={`mobile-nav ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <a className="mob-menu-item" href="#!" >Home</a>
+                <a className="mob-menu-item" href="#!" >Help</a>
+                <a className="mob-menu-item" href="#!" >Search</a>
+                <a className="mob-menu-item" href="#!" >Blog</a>
+                <a className="mob-menu-item" href="#!" >Log In</a>
+                <a className="mob-menu-item" href="#!" >Sign Up</a>
+            </nav>
+
+        </header>
+        
     );
 }
 
